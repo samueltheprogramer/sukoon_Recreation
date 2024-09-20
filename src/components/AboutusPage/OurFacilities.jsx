62ad591709a960e7e7d0b970e726db68631b93d3
Samuel . V <samuelpocom3@gmail.com>
2024-09-21 00:10:00 +0530

    push on [21/09/24]

--- src/components/AboutusPage/OurFacilities.jsx
+++ src/components/AboutusPage/OurFacilities.jsx
@@ -1,4 +1,4 @@
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 import React, { useEffect, useState } from "react";
 
 const OurFacilities = () => {
@@ -7,17 +7,18 @@ const OurFacilities = () => {
   const [button2, setButton2] = useState(false);
   const [button3, setButton3] = useState(false);
   const [button4, setButton4] = useState(false);
-  const [lat, setLat] = useState(28.4595);
-  const [lng, setLng] = useState(77.0266);
+  const [lat, setLat] = useState(28.424882000000004);
+  const [lng, setLng] = useState(77.092248);
 
   const onClickButton1 = () => {
     setButton1(true);
     setButton2(false);
     setButton3(false);
     setButton4(false);
-    setLat(28.4595);
-    setLng(77.0266);
+    setLat(28.424882000000004);
+    setLng(77.092248);
   };
+
   const onClickButton2 = () => {
     setButton1(false);
     setButton2(true);
@@ -26,6 +27,7 @@ const OurFacilities = () => {
     setLat(13.040681374826496);
     setLng(77.56269753373114);
   };
+
   const onClickButton3 = () => {
     setButton1(false);
     setButton2(false);
@@ -34,6 +36,7 @@ const OurFacilities = () => {
     setLat(28.5537576299257);
     setLng(77.23203708465661);
   };
+
   const onClickButton4 = () => {
     setButton1(false);
     setButton2(false);
@@ -57,17 +60,21 @@ const OurFacilities = () => {
     lat: lat,
     lng: lng,
   };
+
+  if (loadError) return;
+  if (!isLoaded) return;
+
   return (
-    <div className="lg:px-20 lg:mb-20 mb-10 sm:mb-[4.5rem] lg:py-10 p-4 flex flex-col gap-3 lg:gap-5 sm:gap-4 ">
+    <div className="lg:px-20 lg:mb-20 mb-10 sm:mb-[4.5rem] lg:py-10 p-4 flex flex-col gap-3 lg:gap-5 sm:gap-4">
       <h1 className="lg:text-5xl text-3xl font-semibold font-serif text-black/90">
         Our Facilities
       </h1>
-      <div className="group w-full lg:flex lg:text-xl lg:gap-5 text-center no-scrollbar flex gap-5  overflow-x-scroll ">
+      <div className="group w-full lg:flex lg:text-xl lg:gap-5 text-center no-scrollbar flex gap-5 overflow-x-scroll">
         <button
           onClick={onClickButton1}
           className={`${
             button1 && "order-first lg:order-none bg-blue-100/70"
-          } w-full lg:py-6 lg:px-16  text-nowrap border p-3  rounded-2xl font-semibold `}
+          } w-full lg:py-6 lg:px-16 text-nowrap border p-3 rounded-2xl font-semibold`}
         >
           Gurgaon
         </button>
@@ -75,7 +82,7 @@ const OurFacilities = () => {
           onClick={onClickButton2}
           className={`${
             button2 && "order-first lg:order-none bg-blue-100/70"
-          } w-full lg:py-6 lg:px-16  text-nowrap border p-3  rounded-2xl font-semibold `}
+          } w-full lg:py-6 lg:px-16 text-nowrap border p-3 rounded-2xl font-semibold`}
         >
           Bengaluru
         </button>
@@ -83,7 +90,7 @@ const OurFacilities = () => {
           onClick={onClickButton3}
           className={`${
             button3 && "order-first lg:order-none bg-blue-100/70"
-          } w-full lg:py-6 lg:px-16  text-nowrap border p-3  rounded-2xl font-semibold `}
+          } w-full lg:py-6 lg:px-16 text-nowrap border p-3 rounded-2xl font-semibold`}
         >
           Delhi
         </button>
@@ -91,7 +98,7 @@ const OurFacilities = () => {
           onClick={onClickButton4}
           className={`${
             button4 && "order-first lg:order-none bg-blue-100/70"
-          } w-full lg:py-6 lg:px-16  text-nowrap border p-3  rounded-2xl font-semibold `}
+          } w-full lg:py-6 lg:px-16 text-nowrap border p-3 rounded-2xl font-semibold`}
         >
           Chhatarpur
         </button>
@@ -101,8 +108,10 @@ const OurFacilities = () => {
           className="googleMap"
           mapContainerClassName="map-container"
           center={center}
-          zoom={10}
-        ></GoogleMap>
+          zoom={15}
+        >
+          <Marker position={center} />
+        </GoogleMap>
       </div>
     </div>
   );
